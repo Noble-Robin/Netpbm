a1f438a1e6aee0191e13725eaec4fe8fedba2b33
robin <robin.noble@ynov.com>
2024-01-09 09:34:09 +0100

    .

--- pbm.go
+++ pbm.go
@@ -1,13 +1,19 @@
-package projet
+package netpbm
 
 import (
 	"bufio"
 	"fmt"
 	"os"
 	"strconv"
-	"Netpbm/struct"
+	
 	
 )
+type PBM struct{
+    Data [][]bool
+    Width, Height int
+    MagicNumber string
+}
+
 
 func ReadPBM(filename string) (*PBM, error){
 	file, err := os.Open(filename)
@@ -17,33 +23,30 @@ func ReadPBM(filename string) (*PBM, error){
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-
-	// Read the header
+	
 	if !scanner.Scan() {
-		return nil, fmt.Errorf("could not read magic number")
+		return nil, fmt.Errorf("peu pas lire le magicnumber")
 	}
-	magicNum := scanner.Text()
+	magicNumber := scanner.Text()
 
 	if !scanner.Scan() {
-		return nil, fmt.Errorf("could not read width and height")
+		return nil, fmt.Errorf("peu pas lire les dimention")
 	}
 	var width, height int
 	_, err = fmt.Sscanf(scanner.Text(), "%d %d", &width, &height)
 	if err != nil {
 		return nil, err
 	}
-
-	// Initialize the image data
+	
 	data := make([][]bool, height)
 	for i := range data {
 		data[i] = make([]bool, width)
 	}
 
-	// Read the image data
 	for y := 0; y < height; y++ {
-		if magicNum == "P4" {
+		if magicNumber == "P4" {
 			if !scanner.Scan() {
-				return nil, fmt.Errorf("could not read image data")
+				return nil, fmt.Errorf("peu pas lire la data de l'image")
 			}
 			row := scanner.Text()
 			for x := 0; x < width; x++ {
@@ -56,7 +59,7 @@ func ReadPBM(filename string) (*PBM, error){
 		} else {
 			for x := 0; x < width; x++ {
 				var value int
-				_, err := fmt.Fscanf(scanner, "%d", &value)
+				_, err := fmt.Fscanf(file, "%d", &value)
 				if err != nil {
 					return nil, err
 				}
@@ -65,5 +68,5 @@ func ReadPBM(filename string) (*PBM, error){
 		}
 	}
 
-	return &PBM{data: data, width: width, height: height,}, nil
+	return &PBM{Data: data, Width: width, Height: height, MagicNumber: magicNumber}, nil
 }
--- struct/struct.go
+++
@@ -1,8 +0,0 @@
-package struct
-
-type PBM struct{
-    data [][]bool
-    width int
-	height int
-    magicNumber string
-}
